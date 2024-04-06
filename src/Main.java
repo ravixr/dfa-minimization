764c959b8e36d35445cba9445cb9795e16e90909
PedroPampolini <pedromendicino25@gmail.com>
2024-04-05 23:38:06 -0300

    xml parser feito

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 class Main {
-    public static void main(String[] args) {
-        DFA.foo();
+    public static void main(String[] args) throws Exception {
+        
+       // XMLParser parser = new XMLParser();
+        DFA dfa = DFA.FromJFLAPXML("automato_test.jff");
+        System.out.println(dfa.toString());
+        dfa.SaveJFLAPXML("automato_test2.jff");
     }
 }
